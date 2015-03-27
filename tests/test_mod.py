f8c33d649e68ce24c2915915f9ab0363065700e5
Amit Kapadia <amit@planet.com>
2015-03-26 21:19:49 -0700

    test spherical coord transform

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -2,9 +2,16 @@
 from cloudshadow import *
 
 
-def test_():
-    """docstring for test_"""
-    pass
+def test_get_spherical_azimuth():
+    assert get_spherical_azimuth(0) == 90
+    assert get_spherical_azimuth(90) == 0
+    assert get_spherical_azimuth(180) == 270
+    assert get_spherical_azimuth(-180) == 270
+    assert get_spherical_azimuth(-90) == 180
+    assert get_spherical_azimuth(0) == 90
+    
 
-def test_find_show_shadows():
-    assert True
+def test_get_spherical_azimuth():
+    assert get_spherical_zenith(90) == 0
+    assert get_spherical_zenith(0) == 90
+    assert get_spherical_zenith(-89) == 179
